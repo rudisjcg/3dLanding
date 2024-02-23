06b4c320527dbac66cbd4d202f69473d80b67d5d
krakatua <rudisjcg@gmail.com>
2024-02-23 08:35:43 -0400

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Quote from './sections/Quote'
+import { GlobalStyle } from './styles/GlobalStyle'
+
+function App() {
+
+  return (
+    <>
+      <GlobalStyle />
+      <Quote />
+    </>
+  )
+}
+
+export default App
