13a609b370d5500a6bc0784e961045babdca48a2
Rudis <82242977+rudisjcg@users.noreply.github.com>
2024-02-25 16:20:49 -0400

    Last changes

--- src/sections/PricingSection.jsx
+++ src/sections/PricingSection.jsx
@@ -1,167 +1,216 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import gsap from "gsap";
 import React from "react";
-import { useLayoutEffect } from "react";
-import { useRef } from "react";
 import styled from "styled-components";
-import v1 from "../assets/video/Scuba Diving - 699.mp4";
-import v2 from "../assets/video/Skate - 49791.mp4";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import Model3 from "../components/Scene3";
+import { AdaptiveDpr, AdaptiveEvents, Environment } from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
+import { useRef } from "react";
+import { useContext } from "react";
+import { ColorContext } from "./../context/ColorContext";
+import { useEffect } from "react";
 
-const Section = styled.section`
+const Container = styled.div`
   width: 100vw;
-  min-height: 100vh;
+  height: 100vh;
   position: relative;
   z-index: 1;
+
   background-color: var(--white);
   overflow: hidden;
 `;
-
-const V1 = styled.video`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
+const Section = styled.div`
+  width: 100vw;
   height: 100vh;
-  object-fit: cover;
-  object-position: bottom;
-  z-index: 2;
-`;
-
-const V2 = styled.video`
-  position: absolute;
-  top: 0;
-  right: 40%;
-  width: 60%;
-  height: 1auto;
-
+  position: relative;
   z-index: 1;
 
-  @media screen and (max-width: 30em) {
-    width: 100%;
-    right: 0;
-    top: 10%;
-  }
+  background-color: "#9BB5CE";
 `;
+const Phone = styled.div`
+  width: 100%;
+  height: 70%;
 
-const TitleContainer = styled.div`
-  width: 50%;
-  height: 100%;
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  position: relative;
+  cursor: grab;
+`;
+
+const Colors = styled.ul`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   align-items: center;
 
   position: absolute;
-  top: 0;
-  right: 0;
+  left: 35%;
+  top: 50%;
+  transform: translate(-50%, -50%);
 
-  & > *:nth-child(2) {
-    margin-left: 6rem;
-  }
-  & > *:nth-child(3) {
-    margin-left: 12rem;
-  }
-
-  @media screen and (max-width: 48em) {
-    top: 60%;
-    right: 2rem;
-  }
-  @media screen and (max-width: 40em) {
-    right: 5rem;
-  }
-  @media screen and (max-width: 30em) {
-    top: 70%;
-    right: 40%;
+  @media screen and (max-width: 64em) {
+    left: 10%;
   }
 `;
 
-const Title = styled.h1`
-  font-size: var(--fontBig);
-  z-index: 5;
-  text-transform: capitalize;
+const Color = styled.li`
+  list-style: none;
+  width: 1.5rem;
+  height: 1.5rem;
+  cursor: pointer;
 
-  @media screen and (max-width: 70em) {
-    font-size: var(--fontxxxl);
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+  margin: 0.5rem 0;
+
+  border: 1px solid var(--dark);
+`;
+const Details = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h2`
+  font-size: var(--fontxl);
+  padding: 0.3rem;
+`;
+
+const SubTitle = styled.h2`
+  font-size: var(--fontmd);
+  font-family: var(--fontR);
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 1rem;
+`;
+
+const Btn = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  margin: 0;
+  padding: 0.4rem 1rem;
+  border-radius: 50px;
+
+  border: none;
+  outline: none;
+
+  background-color: var(--blue);
+  color: var(--white);
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.7;
   }
-  @media screen and (max-width: 48em) {
-    font-size: var(--fontxxl);
+`;
+const BtnLink = styled.a`
+  color: var(--blue);
+  text-decoration: none;
+  margin-left: 1.5rem;
+
+  &:hover {
+    text-decoration: underline;
   }
 `;
 
-const CameraSection = () => {
+const IndicatorText = styled.div`
+  font-size: var(--fontsm);
+  position: absolute;
+  top: 1rem;
+`;
+
+const PricingSection = () => {
   const sectionRef = useRef(null);
 
-  const videoRef1 = useRef(null);
-  const videoRef2 = useRef(null);
-  const titleRef = useRef(null);
+  const { currentColor, changeColorContext } = useContext(ColorContext);
 
-  let elements = gsap.utils.selector(titleRef);
+  useEffect(() => {
+    sectionRef.current.style.backgroundColor = `rgba(${currentColor.rgbColor},0.4)`;
+  }, [currentColor]);
 
-  useLayoutEffect(() => {
-    const Elem = sectionRef?.current;
-    const video1Elem = videoRef1?.current;
-    const video2Elem = videoRef2?.current;
-
-    // pin the section
-    gsap.to(Elem, {
-      scrollTrigger: {
-        trigger: Elem,
-        start: "top top",
-        end: `bottom+=500 bottom`,
-        scrub: 1,
-        pin: true,
-        pinSpacing: true,
-      },
-    });
-
-    let t2 = gsap
-      .timeline({
-        scrollTrigger: {
-          trigger: Elem,
-          start: "top top",
-          end: `bottom+=500 bottom`,
-          scrub: 1,
-        },
-      })
-      .to(video1Elem, { scale: 0.3 }, "key1")
-      .to(video2Elem, { scale: 0.6 }, "key1");
-
-    elements("h1").forEach((el) =>
-      t2.fromTo(
-        el,
-        {
-          scrollTrigger: {
-            trigger: el,
-            start: "top top",
-            end: `bottom bottom`,
-            scrub: 1,
-            // markers: true,
-          },
-          x: 100,
-          opacity: 0,
-        },
-        {
-          x: 0,
-          opacity: 1,
-        }
-      )
-    );
-
-    return () => {
-      if (t2) t2.kill();
+  let updateColor = (color, text, rgbColor) => {
+    const colorObj = {
+      color,
+      text,
+      rgbColor,
     };
-  }, []);
+    changeColorContext(colorObj);
+  };
 
   return (
-    <Section ref={sectionRef}>
-      <V1 ref={videoRef1} src={v1} type="video/mp4" autoPlay muted loop />
-      <V2 ref={videoRef2} src={v2} type="video/mp4" autoPlay muted loop />
-      <TitleContainer ref={titleRef}>
-        <Title>Ready.</Title>
-        <Title>Steady.</Title>
-        <Title>Action.</Title>
-      </TitleContainer>
-    </Section>
+    <Container>
+      <Section ref={sectionRef}>
+        <Phone>
+          <IndicatorText>360&deg; &#x27F2; </IndicatorText>
+          <Canvas camera={{ fov: 14 }}>
+            <ambientLight intensity={1} />
+            <directionalLight intensity={0.4} />
+            <Suspense fallback={null}>
+              <Model3 />
+            </Suspense>
+
+            <Environment preset="night" />
+            <AdaptiveDpr pixelated />
+            <AdaptiveEvents />
+            <OrbitControls enableZoom={false} />
+          </Canvas>
+
+          <Colors>
+            <Color
+              color="#9BB5CE"
+              onClick={() =>
+                updateColor("#9BB5CE", "Sierra Blue", "155, 181, 206")
+              }
+            />
+            <Color
+              color="#F9E5C9"
+              onClick={() => updateColor("#F9E5C9", "Gold", "249, 229, 201")}
+            />
+            <Color
+              color="#505F4E"
+              onClick={() =>
+                updateColor("#505F4E", "Alpine Green", "80, 95, 78")
+              }
+            />
+            <Color
+              color="#574f6f"
+              onClick={() =>
+                updateColor("#574f6f", "Deep Purple", "87, 79, 111")
+              }
+            />
+            <Color
+              color="#A50011"
+              onClick={() => updateColor("#A50011", "Red", "165, 0, 17")}
+            />
+            <Color
+              color="#215E7C"
+              onClick={() => updateColor("#215E7C", "Blue", "33, 94, 124")}
+            />
+          </Colors>
+        </Phone>
+
+        <Details>
+          <SubTitle>iPhone</SubTitle>
+          <Title>14 Pro Max</Title>
+          <SubTitle>From $1099*</SubTitle>
+          <ButtonContainer>
+            <Btn>Buy</Btn>
+            <BtnLink href="#">Learn More &#x2192;</BtnLink>
+          </ButtonContainer>
+        </Details>
+      </Section>
+    </Container>
   );
 };
 
-export default CameraSection;
+export default PricingSection;
